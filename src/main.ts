51de64b169cef08ee0abe26c597732c974837e5e
C00kieMonsta <antoineboxho@gmail.com>
2024-12-12 12:17:13 +0000

    Setup simple ts project

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+function canJump(nums: number[]): boolean {
+	const len = nums.length;
+	for (let index = 0; index < len; index++) {
+		
+	}
+    
+};
+
+// exmample 1 - nums = [2,3,1,1,4]
+const nums_1 = [2,3,1,1,4]
+canJump(nums_1)
+
+// example 2 - nums = [3,2,1,0,4]
+const nums_2 = [3,2,1,0,4]
+canJump(nums_2)
